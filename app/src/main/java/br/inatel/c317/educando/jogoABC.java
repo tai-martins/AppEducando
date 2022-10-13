b66fcc77dfb2f41901b5343302096b556541b6fc
Tainara de Cássia Martins <tainara.martins@inatel.br>
2022-10-13 11:45:53 -0300

    first commit

---
+++ app/src/main/java/br/inatel/c317/educando/jogoABC.java
@@ -0,0 +1,13 @@
+package br.inatel.c317.educando;
+
+    import androidx.appcompat.app.AppCompatActivity;
+    import android.os.Bundle;
+
+public class jogoABC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_abc);
+    }
+}
